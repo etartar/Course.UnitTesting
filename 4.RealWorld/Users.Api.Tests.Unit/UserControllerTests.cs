b1b81ee2a17bf44b2aef085342ef6f9f4434b6fe
Emir TARTAR <emirtartar35@gmail.com>
2024-05-21 21:14:16 +0300

    UsersController test kodlaması tamamlandı.

--- 4.RealWorld/Users.Api.Tests.Unit/UserControllerTests.cs
+++ 4.RealWorld/Users.Api.Tests.Unit/UserControllerTests.cs
@@ -1,11 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Users.Api.Controllers;
+using Users.Api.DTOs;
+using Users.Api.Models;
+using Users.Api.Services;
 
 namespace Users.Api.Tests.Unit;
 
-internal class UserControllerTests
+public class UserControllerTests
 {
+    private readonly UsersController _sut;
+    private readonly IUserService _userService = Substitute.For<IUserService>();
+
+    public UserControllerTests()
+    {
+        _sut = new UsersController(_userService);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnUsers()
+    {
+        // Arrange
+        _userService.GetAllAsync().Returns(Enumerable.Empty<User>().ToList());
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetAll(default);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnUser()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var user = new User()
+        {
+            Id = userId,
+            FullName = "Emir TARTAR"
+        };
+
+        _userService.GetByIdAsync(userId).Returns(user);
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetById(userId, default);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Create_ShouldReturnTrue()
+    {
+        // Arrange
+        var request = new CreateUserDto("Emir TARTAR");
+        _userService.CreateAsync(request).Returns(true);
+
+        // Act 
+        var result = (OkObjectResult)await _sut.Create(request, default);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task DeleteById_ShouldReturnTrue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+
+        _userService.DeleteByIdAsync(userId).Returns(true);
+
+        // Act 
+        var result = (OkObjectResult)await _sut.DeleteById(userId, default);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
 }
